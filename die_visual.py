f7326953964d762da65d97d7063dffc36c5bab9f
Gean Marcos Figueredo Sena <122699523+GeanMFSena@users.noreply.github.com>
2025-12-20 12:11:34 -0300

    adicionando personalizacoes com o plotly

--- die_visual.py
+++ die_visual.py
@@ -22,6 +22,8 @@ for sides in poss_results:
     
 print(frequency)
     
-    
-fig = px.bar(x=poss_results, y= frequency)
+
+# no plotly express diferente do matplotlib eu posso passar o nome dos rotulos das colunas # na linha em que crio o grafico com o labels(rotulos)
+fig = px.bar(x=poss_results, y= frequency,labels={'x':'results','y':'frequency of results'},title='Results of rolling one d6 1,000 times')
+
 fig.show()

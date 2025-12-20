1f4a86cd3c7ba29754ac7bff8f669f156d0b1831
Gean Marcos Figueredo Sena <122699523+GeanMFSena@users.noreply.github.com>
2025-12-19 22:40:26 -0300

    refatorando random walk

--- random_walk.py
+++ random_walk.py
@@ -1,39 +1,34 @@
 import random as rd 
 
 class RandomWalk:
-    '''criando uma classe de passeio aleatorio '''
+    """Uma classe para gerar passeios aleatórios."""
     
-    def __init__(self,num_points=5000):
-        self.num_points = num_points 
-    
-    def fill_walk(self):
-        '''Calcula todos os pontos do passeio'''
-        
+    def __init__(self, num_points=5000):
+        self.num_points = num_points
+        # O passeio começa em (0, 0)
         self.x_values = [0]
         self.y_values = [0]
-        
-        # continua dando passos ate que o passeio atinja a distancia desejada
+
+    def get_step(self):
+        """Calcula a direção e a distância para um passo individual."""
+        direction = rd.choice([1, -1])
+        distance = rd.choice([0, 1, 2, 3, 4])
+        return direction * distance
+
+    def fill_walk(self):
+        """Calcula todos os pontos do passeio."""
         while len(self.x_values) < self.num_points:
             
-            # decide qual decisao tomar, e ate onde 
-            x_direction = rd.choice([1,-1])
-            x_distance = rd.choice([0,1,2,3,4])
-            x_step = x_direction * x_distance
-            
-            y_direction = rd.choice([1,-1])
-            y_distance = rd.choice([0,1,2,3,4])
-            y_step = y_direction * y_distance
-            
-            # rejeita movimentos que nao vao a lugar nenhum 
-            
+            x_step = self.get_step()
+            y_step = self.get_step()
+
+            # Rejeita movimentos que não vão a lugar nenhum
             if x_step == 0 and y_step == 0:
                 continue
-            
-            # calcula a nova posicao 
-            x = self.x_values[-1] + x_step
-            y = self.y_values[-1] + y_step
-            
-            self.x_values.append(x)
-            self.y_values.append(y)
-            
-            
+
+            # Calcula a próxima posição baseada no último valor
+            next_x = self.x_values[-1] + x_step
+            next_y = self.y_values[-1] + y_step
+
+            self.x_values.append(next_x)
+            self.y_values.append(next_y)

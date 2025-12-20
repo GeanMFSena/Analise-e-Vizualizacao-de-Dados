b42019c82b1aa21deabae7c41832d3b6466cf6d3
Gean Marcos Figueredo Sena <122699523+GeanMFSena@users.noreply.github.com>
2025-12-19 21:55:35 -0300

    adicionando personalizacoes na plotagem

--- rw_plot.py
+++ rw_plot.py
@@ -4,15 +4,25 @@ import matplotlib.pyplot as plt
 while True:
     
     # cria um random walk
-    rw = RandomWalk()
+    rw = RandomWalk(100_000)
     rw.fill_walk()
 
-    plt.style.use('seaborn-v0_8')
-
+    # plt.style.use('seaborn-v0_8')
+    plt.style.use('classic')
     fig,ax = plt.subplots()
+    
+    point_numbers = range(rw.num_points)    
 
-    ax.scatter(rw.x_values, rw.y_values, c=rw.y_values, cmap= plt.cm.Blues , s=25)
-
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap= plt.cm.Blues ,edgecolors='none', s=1)
+    # ax.scatter(0,0, c='green',edgecolors='none', s=100)
+    # ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    
+    
+    
+    # remove os eixos 
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    
     # isso passa para o python que queremos que o tamanho do grafico para x e y fiquem do mesmo tamanho
 
     ax.set_aspect('equal')
